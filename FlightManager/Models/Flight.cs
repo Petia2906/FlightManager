c8316a7279b94da99b2272fd424e7c5bb7d677f5
511MK511 <mkacheva511@mail.bg>
2024-03-26 01:24:45 +0200

    Create Reservation Model

--- FlightManager/Models/Flight.cs
+++ FlightManager/Models/Flight.cs
@@ -2,15 +2,19 @@
 {
     public class Flight
     {
-        public string FlightID { get; set; }
+        
+        public int FlightID { get; set; }
         public string FlightFrom { get; set; }
         public string FlightTo { get; set; }
         public DateTime TakeOffTime { get; set; }
         public DateTime LandingTime { get; set; }
         public string PlaneType { get; set; }
-        public string PlaneNumber { get; set; }
+        public int PlaneNumber { get; set; }
         public string PilotName { get; set; }
         public int PlaneCapacity { get; set; }
         public int PlaneBusinessClassCapacity { get; set; }
-    }
-}
+        public Flight()
+        {   
+
+        }
+}   }
---
+++ FlightManager/Models/Reservation.cs
@@ -0,0 +1,20 @@
+﻿namespace FlightManager.Models
+{
+    public class Reservation
+    {
+        
+        public int ReservationID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public int EGN { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Nationality { get; set; }
+        public string TicketType { get; set; }
+        public Reservation()
+        {
+
+        }
+
+    }
+}
