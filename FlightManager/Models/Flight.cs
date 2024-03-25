91adfed318f2fe2fa53556f3c207e3e4acabff45
511MK511 <mkacheva511@mail.bg>
2024-03-25 23:20:29 +0200

    Create Flight Model

---
+++ FlightManager/Models/Flight.cs
@@ -0,0 +1,16 @@
+﻿namespace FlightManager.Models
+{
+    public class Flight
+    {
+        public string FlightID { get; set; }
+        public string FlightFrom { get; set; }
+        public string FlightTo { get; set; }
+        public DateTime TakeOffTime { get; set; }
+        public DateTime LandingTime { get; set; }
+        public string PlaneType { get; set; }
+        public string PlaneNumber { get; set; }
+        public string PilotName { get; set; }
+        public int PlaneCapacity { get; set; }
+        public int PlaneBusinessClassCapacity { get; set; }
+    }
+}
