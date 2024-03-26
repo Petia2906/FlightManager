9db4489d937426adb4601cd6415112682c17a3d4
511MK511 <mkacheva511@mail.bg>
2024-03-26 23:04:09 +0200

    Model changes

--- FlightManager/Models/Reservation.cs
+++ FlightManager/Models/Reservation.cs
@@ -1,16 +1,50 @@
-﻿namespace FlightManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightManager.Models
 {
     public class Reservation
     {
         
         public int ReservationID { get; set; }
+
+        [Display(Name="First Name/Име")]
+        [Required(ErrorMessage="First name required/Името е задължително")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Second name/Презиме")]
+        [Required(ErrorMessage = "Second name required/Презимето е задължително")]
         public string SecondName { get; set; }
+
+        [Display(Name = "Last Name/Фамилия")]
+        [Required(ErrorMessage = "Last name required/Фамилията е задължителна")]
         public string LastName { get; set; }
-        public int EGN { get; set; }
-        public string PhoneNumber { get; set; }
+
+        [Display(Name = "EGN/ЕГН")]
+        [Required(ErrorMessage = "EGN required/ЕГН-то е задължително"),RegularExpression(@"^([0-9]{10})",ErrorMessage="EGN is not valid!/ЕГН-то е невалидно!")]
+        public string EGN { get; set; }
+
+        [Display(Name = "Nationality/Националност")]
+        [Required(ErrorMessage = "Nationality required/Националността е задължителна")]
         public string Nationality { get; set; }
+
+        [Display(Name = "Phone number/Телефон")]
+        [Required(ErrorMessage = "Phone number required/Мобилният телефон е задължителен"),RegularExpression(@"^(+[0-9]{1,3}[0-9]{4,12})",ErrorMessage="Phone number is not valid!/Телефонният номер е невалиден!")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Email/Имейл")]
+        [Required(ErrorMessage = "Email required/Имейлът е задължителен")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Flight number/Номер на полета")]
+        [Required(ErrorMessage = "Flight number required/Номерът на полета е задължителен")]
+        
+        public int FlightID { get; set; }
+
+        [Display(Name = "Tiket type/Вид билет")]
+        [Required(ErrorMessage = "Tiket type required/Видът на билета е задължителен")]
         public string TicketType { get; set; }
+
         public Reservation()
         {
 
